7d8e2536bb2ff5ca9a90a10cdea653bfa46c528e
Keshwrld <vickykesh8@gmail.com>
2025-06-05 03:26:54 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import keyword
+
+print("Hello, world")
+
+print(4)
+
+print("\nCodingal" , 4)
+
+print("\nThe result of 3+5 is :" , 3+5)
+
+#keywords in python
+print("Keywords in python:")
+print(keyword.kwlist)
